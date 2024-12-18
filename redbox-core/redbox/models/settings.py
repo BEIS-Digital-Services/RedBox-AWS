c7a3ffd1a12ff301e53022b59a7c15a9d852cc21
Junaid Vallimohamed <junaid.valllimohamed@ics.gov.uk>
2024-12-18 16:00:13 +0000

    hardcoding port correctly

--- redbox-core/redbox/models/settings.py
+++ redbox-core/redbox/models/settings.py
@@ -207,13 +207,13 @@ class Settings(BaseSettings):
         if opensearch_url.startswith("https://"):
             opensearch_url = opensearch_url[len("https://"):]
 
-        OS_HOST = 443
+        OS_HOST = opensearch_url
         logger.warning(f"OpenSearch is={OS_HOST}")
         #credentials, host_port = opensearch_url.split("@")
         #OS_USERNAME, OS_PASSWORD = credentials.split(":")
         #OS_HOST, OS_PORT = host_port.split(":")
         #OS_PORT = int(OS_PORT)  # Convert port to integer
-        OS_PORT = port
+        OS_PORT = 443
         OS_USERNAME = env.str("OPENSEARCH_USER")
         OS_PASSWORD = env.str("OPENSEARCH_PASSWORD")
 
