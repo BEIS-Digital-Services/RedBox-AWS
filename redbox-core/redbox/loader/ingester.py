fcf2394b785fb1fb68b8b4aa204eb3551e3c08e1
Christos Vrionides <christos.vrionides@beis.gov.uk>
2024-12-20 14:55:34 +0000

    Reverted to Old code to test OpenSearch fix

--- redbox-core/redbox/loader/ingester.py
+++ redbox-core/redbox/loader/ingester.py
@@ -33,7 +33,10 @@ alias = env.elastic_chunk_alias
 if ENVIRONMENT.is_local:
     opensearch_url="https://localhost:9200"
 else:
-    opensearch_url = f"https://{env_vars.str('ELASTIC__COLLECTION_ENPDOINT')}"
+    opensearch_host = env_vars.str('OPENSEARCH_HOST')  # Ensure this includes the endpoint
+    username = env_vars.str('OPENSEARCH_USER')
+    password = env_vars.str('OPENSEARCH_PASSWORD')
+    opensearch_url = f"https://{username}:{password}@{opensearch_host}:443"
 
 def clean_json_metadata(raw_metadata: str) -> str:
     """Clean and extract valid JSON from raw metadata."""
