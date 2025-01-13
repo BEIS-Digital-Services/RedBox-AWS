0a4970a1332920d7550664981c4fcaa1400e43e2
Junaid Vallimohamed <junaid.valllimohamed@ics.gov.uk>
2025-01-13 20:34:54 +0000

    normalise file name for matching

--- django_app/redbox_app/redbox_core/views/document_views.py
+++ django_app/redbox_app/redbox_core/views/document_views.py
@@ -141,10 +141,12 @@ class UploadView(View):
         #logger.warning("For user=%s, active files in DB: %s", user.email, list(active_files))
         #logger.warning("Incoming upload filename: %r", file_name)
 
+        normalized_file_name = uploaded_file.name.replace(" ", "_").lower()
+
         already_exists = File.objects.filter(
             user=user, 
             status__in=[File.Status.complete, File.Status.processing],
-            original_file__iendswith=f"/{file_name}"
+            original_file__iendswith=f"/{normalized_file_name}"
         ).exists()
 
         if already_exists:
